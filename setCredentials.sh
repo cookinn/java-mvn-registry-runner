adac208391ee27feaf97c9be83e38d422eeab90c
RichardInnocent <richjinnocent@gmail.com>
2022-04-07 15:50:47 +0100

    fix: Completed setup of maven settings

--- setCredentials.sh
+++ setCredentials.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 PASSWORD=mvn --encrypt-password ${GITHUB_TOKEN}
 
-echo "<settings>
+echo "
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                      http://maven.apache.org/xsd/settings-1.0.0.xsd">
+
+  <activeProfiles>
+    <activeProfile>github</activeProfile>
+  </activeProfiles>
+
+  <profiles>
+    <profile>
+      <id>github</id>
+      <repositories>
+        <repository>
+          <id>github</id>
+	  <url>https://maven.pkg.github.com/${GITHUB_REPOSITORY}</url>
+	</repository>
+      </repositories>
+    </profile>
+  </profiles>
+
   <servers>
     <id>github</id>
-    <username>${GITHUB_USERNAME}</username>
+    <username>${GITHUB_ACTOR}</username>
     <password>${PASSWORD}</password>
   </servers>
 </settings>" > /root/.m2/settings.xml

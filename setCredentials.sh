475f46b3068557267ce83fcd9cab8b3bf70a01eb
RichardInnocent <richjinnocent@gmail.com>
2022-04-07 17:46:51 +0100

    #patch removed redundant info from script

--- setCredentials.sh
+++ setCredentials.sh
@@ -7,27 +7,11 @@ echo "
   xsi:schemaLocation=\"http://maven.apache.org/SETTINGS/1.0.0
                       http://maven.apache.org/xsd/settings-1.0.0.xsd\">
 
-  <activeProfiles>
-    <activeProfile>github</activeProfile>
-  </activeProfiles>
-
-  <profiles>
-    <profile>
-      <id>github</id>
-      <repositories>
-        <repository>
-          <id>github</id>
-	  <url>https://maven.pkg.github.com/${GITHUB_REPOSITORY}</url>
-	</repository>
-      </repositories>
-    </profile>
-  </profiles>
-
   <servers>
     <id>github</id>
     <username>${GITHUB_ACTOR}</username>
     <password>${GITHUB_TOKEN}</password>
   </servers>
-</settings>" > /root/.m2/settings.xml
+</settings>" > ~/.m2/settings.xml
 
 echo "Credentials set"

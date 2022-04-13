6b9f3a2d279654f58a916db6922bdbfaf2bff77c
RichardInnocent <richjinnocent@gmail.com>
2022-04-13 22:54:56 +0100

    #patch Check if the mandatory environment variables exist to improve debugging

--- setCredentials.sh
+++ setCredentials.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 echo "Setting credentials"
 
+if [[ -z "${GITHUB_ACTOR}" ]]
+then
+  echo "GITHUB_ACTOR environment variable is not set. This should specify the GitHub user that will interact with the Maven repository." >&2
+  exit 1
+fi
+
+if [[ -z "${GITHUB_TOKEN}" ]]
+then
+  echo "GITHUB_TOKEN environment variable is not set. This should specify the token for the GitHub user." >&2
+  exit 1
+fi
+
 echo "
 <settings xmlns=\"http://maven.apache.org/SETTINGS/1.0.0\"
   xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
